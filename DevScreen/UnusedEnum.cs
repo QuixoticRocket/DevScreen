48135632517f0ac763854d1ce05bd87e8bdc46ac
Alex Millar <QuixoticRocket@gmail.com>
2015-11-26 16:55:36 +0000

    Adding a useless enum to test something :pizza:

---
+++ DevScreen/UnusedEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevScreen
+{
+    enum UnusedEnum
+    {
+        Completely = 0,
+        Useless = 1,
+        Enum = 2,
+    }
+}
